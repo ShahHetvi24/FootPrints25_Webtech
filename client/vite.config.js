4e76b6a7eb2b0012ea7ebe99c8b4d4adaeff29cb
Hetvi <shahhetvi2404@gmail.com>
2025-03-08 13:22:57 +0530

    Build related error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensure relative paths work correctly
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000/api/v1",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
